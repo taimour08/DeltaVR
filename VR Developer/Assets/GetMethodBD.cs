31c02802817174e877db0066491c3d040b9d1b34
Taimour <mukhay@ttu.ee>
2024-06-18 17:10:58 +0300

    live-data: get live energy after 2 mins and make separate screens for c02 & temp

--- VR Developer/Assets/GetMethodBD.cs
+++ VR Developer/Assets/GetMethodBD.cs
@@ -10,71 +10,78 @@ using System.Text.RegularExpressions;
 public class GetMethodBD: MonoBehaviour
 {
     TMP_InputField outputArea;
+    TMP_InputField outputArea2;
+    TMP_InputField outputArea2;
 
     // Start is a Unity method called when the script starts running
     void Start()
     {
         // Find and get the UI InputField component named "OutputArea"
-        outputArea = GameObject.Find("OutputArea2").GetComponent<TMP_InputField>();
-        // Find the UI Button component named "GetButton" and add a listener for the click event to call the GetData method
-        GameObject.Find("GetButton2").GetComponent<Button>().onClick.AddListener(GetData);
+        outputArea = GameObject.Find("EnergyOutput").GetComponent<TMP_InputField>();
+        outputArea2 = GameObject.Find("CO2Output").GetComponent<TMP_InputField>();
+        outputArea3 = GameObject.Find("TempOutput").GetComponent<TMP_InputField>();
+        
+        // Start the coroutine to fetch data periodically
+        StartCoroutine(FetchDataPeriodically());
     }
 
-    // GetData method triggered when the button is clicked
-    void GetData() => StartCoroutine(GetData_Coroutine());
+    // Coroutine to fetch data periodically every 2 minutes
+    IEnumerator FetchDataPeriodically()
+    {
+        while (true)
+        {
+            // Fetch data
+            yield return StartCoroutine(GetData_Coroutine());
+            
+            // Wait for 2 minutes before fetching data again
+            yield return new WaitForSeconds(120);
+        }
+    }
 
     // Coroutine for handling data retrieval asynchronously
     IEnumerator GetData_Coroutine()
     {
         // Display "Loading..." in the UI text field
         outputArea.text = "Loading...";
+        outputArea2.text = "Loading...";
+
 
         // Define the URI for the HTTP GET request
-       // string uri = "https://api.openweathermap.org/data/2.5/weather?q=Tartu&appid=fc2dd765b55ad13fd78e622ee10ebf97";
-        string uri = "http://172.17.67.20:8086/query?db=delta&q=SELECT%20*%20FROM%20%22KogEN%22%20ORDER%20BY%20time%20DESC%20LIMIT%201";
+        string uri1 = "http://datareader:notthatsecret777@172.17.67.20:8086/query?db=delta&q=SELECT%20*%20FROM%20%22KogEN%22%20ORDER%20BY%20time%20DESC%20LIMIT%201";
 
-
-        // Set plain text HTTP connection option
-       // UnityWebRequest.allowPlainHttp = UnityWebRequest.PlainHttp.Launch;
-
+        
         // Create a UnityWebRequest for the specified URI
-        using (UnityWebRequest request = UnityWebRequest.Get(uri))
+        using (UnityWebRequest request = UnityWebRequest.Get(uri1))
         {
             // Send the request and wait for a response
             yield return request.SendWebRequest();
 
             // Check if there's a network or HTTP error
             if (request.isNetworkError || request.isHttpError)
+            {
                 // Display the error message in the UI text field
                 outputArea.text = request.error;
-            else {
-                // Display the downloaded text in the UI text field
-                //request.downloadHandler.text;   accepts string
-        
-                  string jsonAsText = request.downloadHandler.text;
-                  string pattern = @"\d+\.\d+";      // Regular expression to match floating point numbers
-        
-        // Find all matches in the string
-        MatchCollection matches = Regex.Matches(jsonAsText, pattern);
-
-        // Convert matches to a list of doubles
-        var values = matches.Cast<Match>().Select(m => double.Parse(m.Value)).ToList();
-
-        // Find the maximum value
-        double maxValue = values.Max();
-
-        string unit = "Energy: ";
-
-        outputArea.text = unit + maxValue;
             }
-                // Start if 
+            else
+            {
+                // Display the downloaded text in the UI text field
+                string jsonAsText = request.downloadHandler.text;
+                string pattern = @"\d+\.\d+"; // Regular expression to match floating point numbers
 
-                // extract Num & Unit, put into var.
+                // Find all matches in the string
+                MatchCollection matches = Regex.Matches(jsonAsText, pattern);
 
-                // End if 
-                
-                //outputArea.text = Unit + Numerical;
-                
+                // Convert matches to a list of doubles
+                var values = matches.Cast<Match>().Select(m => double.Parse(m.Value)).ToList();
+
+                // Find the maximum value
+                double maxValue = values.Max();
+
+                string unit = "Energy: ";
+
+                outputArea.text = unit + maxValue;
+                outputArea2.text = unit + maxValue;
+            }
         }
     }
 }
