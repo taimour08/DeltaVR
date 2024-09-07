d3fc4acce61a2b2f4e16d5d1265ce303d6211b6d
Taimour <mukhay@ttu.ee>
2024-09-07 15:35:11 +0300

    data being shown in all rooms. need to correct the index of the config to display data according to each room

--- VR Developer/Assets/Scripts/Lecture Halls/GetMethod203.cs
+++ VR Developer/Assets/Scripts/Lecture Halls/GetMethod203.cs
@@ -3,7 +3,7 @@ public class GetMethod203 : GetMethodBD1
     void Start()
     {
         urlSetIndex = 2; 
-        outputName = "TheOutput203";
+        outputName = "Output203";
         nextName = "Next203";
         prevName = "Prev203";
         base.Start();
